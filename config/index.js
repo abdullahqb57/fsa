3463a96fb79109df370b848f0e9581cfd86b4dab
Mohammed <abdullahqb57@gmail.com>
2025-12-25 09:51:25 +0530

    CRUD operations complete setup

--- config/index.js
+++ config/index.js
@@ -1,7 +1,6 @@
 const config = {
     dbConStr : 'mongodb://localhost:27017/fsa-b3',
     jwtSecret: 'jhga.78jhuhj',
-    // dbConStr : "mongodb+srv://abdullahqb57_db_user:FDQva5GxU4llCUVu@cluster0.ufojsqw.mongodb.net/",
     PORT : 3000
 }
 
